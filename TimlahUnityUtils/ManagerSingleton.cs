02e3e7f9957e3d9663f9cd980c8afdd756210b37
TimothyFothergill <timothy.fothergill@digital.hmrc.gov.uk>
2024-06-08 11:23:11 +0100

    initial commit

---
+++ TimlahUnityUtils/ManagerSingleton.cs
@@ -0,0 +1,12 @@
+﻿namespace TimlahUnityUtils;
+
+using UnityEngine;
+
+public class ManagerSingleton<T> : MonoBehaviour where T : Component
+{
+        public static T _instance {get; private set;}
+
+    void Awake() {
+        if(_instance != null && _instance != this as T){Destroy(this as T);}else{_instance = this as T;}
+    }        
+}
